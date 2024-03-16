63a5af20613619bf4d4e8f921a9ecaab0d629b83
Fadlan Sayyidul Anam <inifadlan7@gmail.com>
2024-03-16 17:05:20 +0700

    initial commit

---
+++ src/main/java/com/project/helpdesk/ItHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.project.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItHelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/helpdesk/ItHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
